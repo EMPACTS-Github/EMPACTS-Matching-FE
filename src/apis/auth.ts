59a1f7ffbfa13cf82242571da109bf607ba77452
gagasieuga <na.ph0304@gmail.com>
2025-01-27 05:52:16 +0700

    integrate signup api

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -4,3 +4,43 @@ import axiosInstance from "."
 export const loginWithGoogleAPI = () => {
   window.location.href = 'http://localhost:3001/api/auth/google/login'
 }
+
+export const email_signup = async (email: string) => {
+  const response = await axiosInstance.post('/auth/verify-email', { email })
+  return response.data
+}
+
+export const verify_OTP = async (email: string, otpCode: string) => {
+  const response = await axiosInstance.post('/auth/verify-otp', { email, otpCode })
+  return response.data
+}
+
+export const create_new_password = async (email: string, password: string) => {
+  const response = await axiosInstance.post('/auth/create-password', { email, password })
+  return response.data
+}
+
+export const create_new_profile = async (email: string, avtUrl: string, name: string) => {
+  const response = await axiosInstance.post('/auth/create-profile', { email, avtUrl, name })
+  return response.data
+}
+
+export const email_signin = async (email: string) => {
+  const response = await axiosInstance.post('/auth/sign-in', { email })
+  return response.data
+}
+
+export const send_forgot_password_otp = async (email: string) => {
+  const response = await axiosInstance.post('/auth/password/otp', { email })
+  return response.data
+}
+
+export const verify_forgot_password_otp = async (email: string, otpCode: string) => {
+  const response = await axiosInstance.post('/auth/password/otp-verify', { email, otpCode })
+  return response.data
+}
+
+export const reset_password = async (email: string, password: string) => {
+  const response = await axiosInstance.post('/auth/password/reset', { email, password })
+  return response.data
+}
