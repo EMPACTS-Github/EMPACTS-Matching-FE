8a66cbb1e8d4c6c16aae52d85a7e8e5e2857459e
ghostoninternet <daosyphuc288@gmail.com>
2025-04-12 16:36:36 +0700

    refactor: Homepage

--- src/containers/Home/StartupCard.tsx
+++ src/containers/Home/StartupCard.tsx
@@ -2,30 +2,24 @@ import React from 'react';
 import Image from 'next/image';
 import GroupIcon from '/public/assets/group.png';
 import LabelIcon from '/public/assets/label.png';
-import AvatarPlaceholder from '/public/assets/avatar-placeholder.png';
+import { HomepageStartup } from '@/interfaces/startup';
 
-interface StartupCardProps {
-  id: number;
-  logoUrl?: string;
-  name?: string;
-  description?: string;
-  memberCount?: number;
-  category?: string;
+interface StartupCardProps extends HomepageStartup {
   onClick: (id: number) => void;
 }
 
 const StartupCard: React.FC<StartupCardProps> = ({
-  id = -1,
-  logoUrl = AvatarPlaceholder,
-  name = 'Startup name',
-  description = 'Make beautiful websites regardless of your design experience. Make beautiful websites regardless of your design experience.',
-  memberCount = 2,
-  category = 'Zero Hunger',
+  id,
+  avt_url,
+  name,
+  description,
+  member_qty,
+  category,
   onClick,
 }) => {
   return (
     <div className="w-[300px] h-[365px] p-5 bg-white rounded-lg flex flex-col items-center gap-5 shadow-md cursor-pointer" onClick={() => onClick(id)}>
-      <Image src={logoUrl} alt="Startup Logo" width={120} height={120} className="object-cover" />
+      <Image src={avt_url} alt="Startup Logo" width={120} height={120} className="object-cover" />
       <h2 className="font-inter font-semibold text-lg text-gray-900 text-center">{name}</h2>
       <p className="font-inter text-sm font-normal text-gray-500 text-center flex-grow overflow-hidden text-ellipsis">
         {description}
@@ -40,7 +34,7 @@ const StartupCard: React.FC<StartupCardProps> = ({
         <div className="flex items-center w-1/2 gap-1">
           <Image src={GroupIcon} alt="Members" width={24} height={24} className="object-cover" />
           <span className="font-inter font-semibold text-base text-black text-center">
-            {memberCount} Members
+            {member_qty} Members
           </span>
         </div>
       </div>
--- src/interfaces/startup.ts
+++ src/interfaces/startup.ts
@@ -1,15 +1,29 @@
+export interface HomepageStartup {
+  id: number;
+  name: string;
+  description?: string;
+  category: string;
+  avt_url: string;
+  member_qty: number;
+}
+
 export interface Startup {
-    id: number;
-    name: string;
-    avt_url?: string;
-    description?: string;
-    category?: string;
-    startup_link?: string;
-    location_based: string;
-    member_qty?: number;
+  id: number;
+  name: string;
+  avt_url?: string;
+  description?: string;
+  category?: string;
+  startup_link?: string;
+  location_based: string;
+  member_qty?: number;
+}
+
+export interface Category {
+  id: string;
+  label: string;
 }
 
 export interface Member {
-    email: string;
-    title: string;
+  email: string;
+  title: string;
 }
