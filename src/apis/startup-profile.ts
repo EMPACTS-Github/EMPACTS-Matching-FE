47e1cfe65ab14f1fe5bce060c6943a38621fed44
ghostoninternet <daosyphuc288@gmail.com>
2025-04-23 13:30:51 +0700

    feature: Startup profile list

--- src/apis/startup-profile.ts
+++ src/apis/startup-profile.ts
@@ -1,6 +1,5 @@
 "use-client";
 import axiosInstance from ".";
-import axios from "axios";
 
 export const startup_profile_detail = async (id: string) => {
     const response = await axiosInstance.get(`/startup-profile/${id}`);
