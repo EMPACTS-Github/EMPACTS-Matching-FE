ef6dcaecf4ccb93d6b154912b430c4a11d2da602
ghostoninternet <daosyphuc288@gmail.com>
2025-05-02 17:15:09 +0700

    feature: Alert user login as when response to invitation

---
+++ src/containers/StartupInvitation/AlertLoginAs.tsx
@@ -0,0 +1,42 @@
+import { Button } from '@heroui/react'
+import React from 'react'
+
+type Props = {
+  email: string,
+  inviteeEmail: string,
+  onLoginAsDifferentAccount: () => void,
+  onCancelLoginAsDifferentAccount: () => void,
+}
+
+const AlertLoginAs = ({ email, inviteeEmail, onLoginAsDifferentAccount, onCancelLoginAsDifferentAccount }: Props) => {
+  return (
+    <div className="w-full flex justify-center items-center mt-10">
+      <div className="rounded-xl p-8 bg-white flex flex-col gap-2 justify-center items-center">
+        <p className="text-xl text-center font-[600] text-[#09090B]">
+          {"You're login as"} <span className="text-xl text-center font-[600] text-[#9200FE]">{email}</span>
+        </p>
+        <p className="text-medium text-center font-[600] text-[#09090B]">
+          {"Please login as"} <span className="text-medium text-center font-[600] text-[#9200FE]">{inviteeEmail}</span> {"to continue"}
+        </p>
+        <div>
+          <Button
+            onPress={onCancelLoginAsDifferentAccount}
+            variant="solid"
+            className="bg-white rounded-lg pl-6 pr-6 border-1 border-[#E4E4E7] mr-5"
+          >
+            Cancel
+          </Button>
+          <Button
+            onPress={onLoginAsDifferentAccount}
+            variant="solid"
+            className="bg-[#9200FE] rounded-lg pl-6 pr-6 text-white"
+          >
+            {`Login in as ${inviteeEmail}`}
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AlertLoginAs
