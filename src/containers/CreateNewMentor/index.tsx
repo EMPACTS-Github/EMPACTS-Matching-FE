d7aeeabee477d17e575a1104ebb6ffcd6649be64
gagasieuga <na.ph0304@gmail.com>
2025-08-04 15:33:02 +0700

    refactor mentor profile

--- src/containers/CreateNewMentor/index.tsx
+++ src/containers/CreateNewMentor/index.tsx
@@ -1,23 +1,25 @@
 "use client";
 import React, { useState } from 'react';
-import HeaderSection from './HeaderSection';
-import ProfilePictureUpload from './ProfilePictureUpload';
-import MentorNameSection from './MentorNameSection';
-import LocationBasedSection from './LocationBasedSection';
-import SDGGoalSection from './SDGGoalSection';
-import ActionButtons from './ActionButtons';
 import { create_mentor_profile } from '@/apis/mentor';
 import { LanguagesSpoken } from '@/constants/common';
 import { SkillOffered } from '@/constants/skillOffered';
 import { addToast } from '@heroui/react';
 import * as changeCase from "change-case";
-import { PROVINCES } from '@/constants/provinces';
 import { updateAttachment } from "@/apis/upload";
 import { useRouter } from 'next/navigation';
 import { UPLOAD_OWNER_TYPE } from '@/constants/upload';
-import LanguagesSpokenSection from './LanguagesSpokenSection';
-import DescriptionSection from './DescriptionSection';
-import SkillOfferedSection from './SkillOfferedSection';
+import LoadingOverlay from '@/components/common/LoadingOverlay';
+import FormFieldWithLabel from '@/components/FormInput/FormFieldWithLabel';
+import FormTextareaWithLabel from '@/components/FormInput/FormTextareaWithLabel';
+import FormSelectWithLabel from '@/components/FormInput/FormSelectWithLabel';
+import FormMultiSelectWithLabel from '@/components/FormInput/FormMultiSelectWithLabel';
+import ImageUploadWithLabel from '@/components/FormInput/ImageUploadWithLabel';
+import AuthButton from '@/components/common/AuthButton';
+import UploadAvatar from "/public/assets/upload_avatar.svg";
+import provinces from '@/utils/data/provinces.json';
+import sdgGoals from '@/utils/data/sdgGoals.json';
+import languages from '@/utils/data/languages.json';
+import skill_offered from '@/utils/data/skillOffered.json';
 
 function CreateNewMentor() {
   const [mentorName, setMentorName] = useState('');
@@ -125,40 +127,114 @@ function CreateNewMentor() {
     }
   };
 
+  const helperText = `Your profile could be found with username ${mentorUsername !== '' ? mentorUsername : '@mentor_name'}. You can change it later in Settings`;
+
   return (
     <div className="w-full flex justify-center items-center min-h-screen relative">
-      {loading && (
-        <div className="absolute inset-0 flex justify-center items-center bg-white bg-opacity-75 z-50">
-          <div className="loader"></div>
-        </div>
-      )}
+      <LoadingOverlay isVisible={loading} />
       <div className="flex flex-col w-2/3 p-8 bg-white rounded-lg shadow-md space-y-4">
-        <HeaderSection />
-        <ProfilePictureUpload onImageUpload={handleChangeImage} />
-        <MentorNameSection
-          mentorName={mentorName}
-          mentorUsername={mentorUsername}
-          onMentorNameChange={setMentorName}
-          onChangeMentorUsername={handleChangeMentorUsername}
+        {/* Header Section */}
+        <div className="flex flex-col items-center gap-2 w-full">
+          <h1 className="text-[28px] font-bold text-secondary leading-[109%] text-center">
+            Mentor profile
+          </h1>
+          <p className="text-sm text-empacts-grey-100 leading-[120%] text-center">
+            Access to your desired mentor
+          </p>
+        </div>
+
+        {/* Profile Picture Upload */}
+        <ImageUploadWithLabel
+          label="Upload a mentor profile picture"
+          onImageUpload={handleChangeImage}
+          defaultImage={UploadAvatar}
+          ownerType={UPLOAD_OWNER_TYPE.MENTOR}
         />
-        <LocationBasedSection selectedLocation={location} onChange={setLocation} />
-        <DescriptionSection
-          description={description}
-          onDescriptionChange={handleDescriptionChange}
+
+        {/* Mentor Name */}
+        <FormFieldWithLabel
+          label="Mentor name"
+          value={mentorName}
+          onChange={(value) => {
+            setMentorName(value);
+            handleChangeMentorUsername(value);
+          }}
+          placeholder="Mentor name"
+          helperText={helperText}
+          required
         />
-        <LanguagesSpokenSection
-          languagesSpoken={languagesSpoken}
-          onLanguagesSpokenChange={handleLanguagesSpokenChange}
+
+        {/* Location Based */}
+        <FormSelectWithLabel
+          label="Location Based"
+          value={location}
+          onChange={setLocation}
+          options={provinces}
+          placeholder="Select a location"
+          required
         />
-        <SkillOfferedSection
-          skillOffered={skillOffered}
-          onSkillOfferedChange={handleSkillOfferedChange}
+
+        {/* Description */}
+        <FormTextareaWithLabel
+          label="Description"
+          value={description}
+          onChange={handleDescriptionChange}
+          placeholder="Add your description here..."
+          rows={6}
+          required
         />
-        <SDGGoalSection
-          selectedGoals={selectedGoals}
-          onGoalsChange={setSelectedGoals}
+
+        {/* Languages Spoken */}
+        <FormMultiSelectWithLabel
+          label="Languages Spoken"
+          value={languagesSpoken}
+          onChange={(value) => handleLanguagesSpokenChange(value as LanguagesSpoken)}
+          options={languages}
+          placeholder="Select languages"
+          required
         />
-        <ActionButtons onCancel={handleCancelCreateProfile} onCreate={handleCreateProfile} />
+
+        {/* Skill Offered */}
+        <FormMultiSelectWithLabel
+          label="Skill Offered"
+          value={skillOffered}
+          onChange={(value) => handleSkillOfferedChange(value as SkillOffered)}
+          options={skill_offered}
+          placeholder="Select Skill Offered"
+          required
+        />
+
+        {/* SDG Goals */}
+        <FormMultiSelectWithLabel
+          label="SDG Goals"
+          value={selectedGoals}
+          onChange={setSelectedGoals}
+          options={sdgGoals}
+          placeholder="Select SDG Goals"
+          required
+        />
+
+        {/* Action Buttons */}
+        <div className="flex flex-row justify-between w-full gap-8 h-12">
+          <div className="flex-1">
+            <AuthButton
+              onClick={handleCancelCreateProfile}
+              variant="bordered"
+              color="default"
+              className="w-full h-[48px] border border-secondary bg-white text-black"
+            >
+              <span className="text-base font-medium">Cancel</span>
+            </AuthButton>
+          </div>
+          <div className="flex-1">
+            <AuthButton
+              onClick={handleCreateProfile}
+              className="w-full h-[48px]"
+            >
+              <span className="text-base font-medium">Create New</span>
+            </AuthButton>
+          </div>
+        </div>
       </div>
     </div>
   )
