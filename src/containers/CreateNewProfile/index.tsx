fa0e5859c63ca45a3bdb7090a1f21996d8b4d1a7
gagasieuga <na.ph0304@gmail.com>
2025-08-15 13:05:41 +0700

    refactor profile

--- src/containers/CreateNewProfile/index.tsx
+++ src/containers/CreateNewProfile/index.tsx
@@ -1,8 +1,46 @@
+'use client';
 import React from 'react';
-import ProfileOption from './ProfileOption';
-import ProfileHeader from './ProfileHeader';
+import { useRouter } from 'next/navigation';
+import RightArrow from '/public/assets/arrow-right.svg';
+import Image from 'next/image';
+import Button from '@/components/Button/Button';
 
 function CreateNewProfile() {
+  const router = useRouter();
+
+  const handleChooseOption = (link: string) => {
+    router.push(link);
+  };
+
+  const ProfileHeader = ({ title, subtitle }: { title: string; subtitle: string }) => (
+    <div className="p-4 text-center mb-6">
+      <h3 className="text-xl font-bold text-gray-800">{title}</h3>
+      <p className="text-gray-500 text-sm">{subtitle}</p>
+    </div>
+  );
+
+  const ProfileOption = ({ title, description, link }: { title: string; description: string; link: string }) => (
+    <Button
+      variant="ghost-full"
+      className="justify-between p-4 py-8 border-b border-gray-200 hover:bg-gray-50"
+      onClick={() => handleChooseOption(link)}
+    >
+      <div className="flex items-center flex-1">
+        <div className="ml-4 text-left">
+          <p className="font-bold text-gray-800">{title}</p>
+          <p className="text-sm text-gray-500">{description}</p>
+        </div>
+      </div>
+      <Image 
+        src={RightArrow} 
+        className="text-gray-400 text-lg" 
+        alt="Arrow right" 
+        width={24} 
+        height={24} 
+      />
+    </Button>
+  );
+
   return (
     <div className="flex justify-center items-center w-full mt-16">
       <div className="max-w-lg w-full mx-auto p-8 rounded-lg shadow-lg bg-white flex flex-col">
