63bf6d45bf0419c336e9426c826ff7a5b4cb182e
gagasieuga <na.ph0304@gmail.com>
2025-08-24 21:58:09 +0700

    refactor invite flow for mentor and startup

--- src/components/Card/MatchingMentorCard.tsx
+++ src/components/Card/MatchingMentorCard.tsx
@@ -1,10 +1,10 @@
-import Image from 'next/image';
 import React from 'react';
 import { Card } from '@heroui/card';
 import TextLine from '@/components/common/TextLine';
+import Avatar from '@/components/Avatar/Avatar';
 import { capitalizeFirstLetter } from '@/utils/capitalizeFirstLetter';
 import { MATCHING_STATUS } from '@/constants/matching';
-import { Avatar } from '@heroui/react';
+import { PROFILE_MESSAGES } from '@/constants';
 
 interface MatchingMentorCardProps {
   name: string;
@@ -25,30 +25,25 @@ const MatchingMentorCard: React.FC<MatchingMentorCardProps> = ({
     <Card
       fullWidth
       isPressable
-      className="flex flex-row gap-2 rounded-md px-4 py-4 items-center shadow-sm"
+      className="flex flex-row gap-small rounded-md px-regular py-regular items-center shadow-sm"
       onPress={onCardClick}
     >
       <div className="w-[25%]">
         <Avatar
+          variant="default-lg"
           src={avatarUrl}
           alt={`${name}'s avatar`}
-          color="primary"
-          isBordered
-          size="lg"
-          radius="full"
-          className="bg-white"
         />
       </div>
       <div className="flex flex-col justify-start w-[75%]">
-        <div className="text-lg font-semibold text-black text-left">{name}</div>
-        <TextLine text={location} className="text-gray-600 text-sm text-left line-clamp-1" />
-        <div className="flex gap-2 items-center mt-2">
-          <div className="font-semibold">Status:</div>
-          <div
-            className={`font-semibold ${status === MATCHING_STATUS.ACCEPTED ? 'text-success' : 'text-gray-500'}`}
-          >
-            {capitalizeFirstLetter(status)}
-          </div>
+        <TextLine text={name} className="text-lg font-semibold text-secondary text-left" />
+        <TextLine text={location} className="text-neutral-50 text-sm text-left line-clamp-1" />
+        <div className="flex gap-small items-center mt-small">
+          <TextLine text={PROFILE_MESSAGES.STATUS} className="font-semibold text-secondary" />
+          <TextLine
+            text={capitalizeFirstLetter(status)}
+            className={`font-semibold ${status === MATCHING_STATUS.ACCEPTED ? 'text-success' : 'text-neutral-50'}`}
+          />
         </div>
       </div>
     </Card>
