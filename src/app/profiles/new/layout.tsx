1a0afbd83c78964761a6e7ef7c70bcd29e38b825
gagasieuga <na.ph0304@gmail.com>
2025-08-27 19:22:36 +0700

    resolve comment

--- src/app/profiles/new/layout.tsx
+++ src/app/profiles/new/layout.tsx
@@ -2,11 +2,13 @@
 import Header from '@/components/Header';
 import React from 'react';
 
-export default function ProfilesNewLayout({ children }: { children: React.ReactNode }) {
+const ProfilesNewLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Header />
       {children}
     </div>
   );
-}
+};
+
+export default ProfilesNewLayout;
