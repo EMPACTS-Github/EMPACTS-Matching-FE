604d164fdfaae51a0deba9a58ec2f152b1661f46
ghostoninternet <daosyphuc288@gmail.com>
2025-09-18 23:22:05 +0700

    Add host info in upcoming meeting item

--- src/containers/Matching/UpcomingMeetings/Content/UpcomingMeetingItem/MeetingDetail/index.tsx
+++ src/containers/Matching/UpcomingMeetings/Content/UpcomingMeetingItem/MeetingDetail/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import AttendeeInfo from './AttendeeInfo';
+import HostInfo from './HostInfo';
 
 const MeetingDetail = () => {
   return (
     <div className='flex gap-10 max-w-[790px]'>
       <AttendeeInfo />
+      <HostInfo />
     </div>
   )
 }
