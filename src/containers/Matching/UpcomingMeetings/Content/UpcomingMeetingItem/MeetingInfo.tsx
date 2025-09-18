5b4df4d98dab126132053d699c4bd52969e690eb
ghostoninternet <daosyphuc288@gmail.com>
2025-09-18 23:44:44 +0700

    Add join meeting action ui in upcoming meeting info

--- src/containers/Matching/UpcomingMeetings/Content/UpcomingMeetingItem/MeetingInfo.tsx
+++ src/containers/Matching/UpcomingMeetings/Content/UpcomingMeetingItem/MeetingInfo.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import MeetingDetail from './MeetingDetail';
+import JoinMeetingAction from './JoinMeetingAction';
 
 const MeetingInfo = () => {
   return (
-    <div className='max-w-[1040px] flex justify-between items-center min-h-[92px]'>
+    <div className='max-w-[1040px] flex justify-between min-h-[92px]'>
       <MeetingDetail />
+      <JoinMeetingAction />
     </div>
   );
 };
