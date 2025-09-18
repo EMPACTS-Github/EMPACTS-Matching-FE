79c7c02e64ec34231e5a7537a742de614e732456
ghostoninternet <daosyphuc288@gmail.com>
2025-09-19 01:02:41 +0700

    Add upcoming meeting members modal

--- src/containers/Matching/UpcomingMeetings/Content/UpcomingMeetingItem/MeetingAction/index.tsx
+++ src/containers/Matching/UpcomingMeetings/Content/UpcomingMeetingItem/MeetingAction/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import { Button } from '@heroui/react';
+import MemberModal from './MemberModal';
 
 const MeetingAction = () => {
+  const [isMemberModalOpen, setIsMemberModalOpen] = useState(false);
+
+  const handleOpenMemberModal = () => {
+    setIsMemberModalOpen(true);
+  }
+
+  const handleCloseMemberModal = () => {
+    setIsMemberModalOpen(false);
+  }
+
   return (
     <div className='flex justify-between'>
       <div className='w-full flex justify-center'>
@@ -10,7 +22,7 @@ const MeetingAction = () => {
           className='bg-black text-white'
           size='lg'
           radius='lg'
-          onPress={() => {}}
+          onPress={handleOpenMemberModal}
         >
           <p className='text-base font-bold leading-6'>Meeting Members</p>
         </Button>
@@ -26,6 +38,7 @@ const MeetingAction = () => {
           <p className='text-base font-bold leading-6'>Cancel Meeting</p>
         </Button>
       </div>
+      <MemberModal isOpen={isMemberModalOpen} onOpenChange={handleCloseMemberModal} />
     </div>
   );
 };
