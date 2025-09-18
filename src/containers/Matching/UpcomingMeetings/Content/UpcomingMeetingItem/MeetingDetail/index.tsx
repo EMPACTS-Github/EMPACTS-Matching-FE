8b512b9f651c7ddd94534bfabad43df1a4f3474a
ghostoninternet <daosyphuc288@gmail.com>
2025-09-18 22:56:14 +0700

    Add attendee info for upcoming meeting item

---
+++ src/containers/Matching/UpcomingMeetings/Content/UpcomingMeetingItem/MeetingDetail/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AttendeeInfo from './AttendeeInfo';
+
+const MeetingDetail = () => {
+  return (
+    <div className='flex gap-10 max-w-[790px]'>
+      <AttendeeInfo />
+    </div>
+  )
+}
+
+export default MeetingDetail;
---
+++ src/containers/Matching/UpcomingMeetings/Content/UpcomingMeetingItem/MeetingInfo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MeetingDetail from './MeetingDetail';
+
+const MeetingInfo = () => {
+  return (
+    <div className='max-w-[1040px] flex justify-between items-center min-h-[92px]'>
+      <MeetingDetail />
+    </div>
+  );
+};
+
+export default MeetingInfo;
