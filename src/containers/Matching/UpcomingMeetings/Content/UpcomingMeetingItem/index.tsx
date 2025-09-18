0c8ee98a1ee2563821b7da14dd90b02ddc75fb1d
ghostoninternet <daosyphuc288@gmail.com>
2025-09-19 00:06:08 +0700

    Add upcoming meeting action ui

--- src/containers/Matching/UpcomingMeetings/Content/UpcomingMeetingItem/index.tsx
+++ src/containers/Matching/UpcomingMeetings/Content/UpcomingMeetingItem/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import MeetingInfo from './MeetingInfo';
+import MeetingAction from './MeetingAction';
 
 const UpcomingMeetingItem = () => {
   return (
-    <div className='max-w-[1072px] rounded-md flex flex-col gap-3 p-4 border-[1px] border-neutral-50'><MeetingInfo /></div>
+    <div className='max-w-[1072px] rounded-md flex flex-col gap-6 p-4 border-[1px] border-neutral-50'>
+      <MeetingInfo />
+      <MeetingAction />
+    </div>
   );
 }
 
