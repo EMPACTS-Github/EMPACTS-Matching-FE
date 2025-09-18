304967cd092ae278a226ac0fa50b7f09013dadc4
ghostoninternet <daosyphuc288@gmail.com>
2025-09-18 22:56:14 +0700

    Add attendee info for upcoming meeting item

---
+++ src/containers/Matching/UpcomingMeetings/Content/UpcomingMeetingItem/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import MeetingInfo from './MeetingInfo';
+
+const UpcomingMeetingItem = () => {
+  return (
+    <div className='max-w-[1072px] rounded-md flex flex-col gap-3 p-4 border-[1px] border-neutral-50'><MeetingInfo /></div>
+  );
+}
+
+export default UpcomingMeetingItem;
