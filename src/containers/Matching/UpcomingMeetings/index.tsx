9b0197b858536a4cf31680655fc17e05099f6de6
lpnizmeee <lpnizmeee@lpnizmees-MacBook.local>
2025-09-19 16:30:07 +0700

    merge src from Nam and Phuc

--- src/containers/Matching/UpcomingMeetings/index.tsx
+++ src/containers/Matching/UpcomingMeetings/index.tsx
@@ -1,14 +1,9 @@
-import React from 'react'
-import Header from './Header'
-import Content from './Content'
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
 
 const UpcomingMeetings = () => {
-  return (
-    <div className='p-6 flex flex-col gap-8 bg-neutral-20 shadow-[0px_0px_8px_0px_rgba(10, 20, 57, 0.07)] max-h-[576px] rounded-xl'>
-      <Header />
-      <Content />
-    </div>
-  )
-}
+  return <Content />;
+};
 
-export default UpcomingMeetings
+export default UpcomingMeetings;
